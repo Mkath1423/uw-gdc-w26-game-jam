6fc38c229e8b6a7ba6d93071968d5b4d83164bbe
Lex Stapleton <lex.stapleton1423@gmail.com>
2026-01-31 11:52:38 -0500

    moveing computers

--- fitting-in/core/autoloads/game_state.gd
+++ fitting-in/core/autoloads/game_state.gd
@@ -3,6 +3,8 @@ class_name _GameState extends Node
 var stress : float = 0
 var max_stress : float = 100
 
+var sem_is_stressed : int = 0
+
 signal stress_updated
 signal stress_limit_reached
 
@@ -16,9 +18,21 @@ func add_stress(amount : float):
     if stress > max_stress:
         stress_limit_reached.emit()
 
+    stress = clamp(stress, 0, INF)
+
+func set_stressed():
+    sem_is_stressed += 1
+    stress_updated.emit()
+
+func unset_stressed():
+    sem_is_stressed -= 1
+    stress_updated.emit()
+
+func is_stressed():
+    return sem_is_stressed > 0
+
 func reset():
     stress = 0
     max_stress = 100
 
     state_reset.emit()
-
