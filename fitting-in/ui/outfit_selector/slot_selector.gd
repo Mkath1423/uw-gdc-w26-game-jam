bc83e954a422e1b1b910d569afbfc7e10f3f2ee8
Lex Stapleton <lex.stapleton1423@gmail.com>
2026-02-01 16:19:53 -0500

    keyinputs

--- fitting-in/ui/outfit_selector/slot_selector.gd
+++ fitting-in/ui/outfit_selector/slot_selector.gd
@@ -2,11 +2,23 @@ extends Control
 
 @export var type : BaseAccessory.AccessoryType
 
+@export var pos_action : String
+@export var neg_action : String
+
 @onready var icon : TextureRect = $slot_selector/MarginContainer/panel/CenterContainer/TextureRect
 
 func _ready():
 	GameState.player_inventory.outfit_changed.connect(on_outfit_change)
 	on_outfit_change()
+	
+
+func _process(_delta):
+	if Input.is_action_just_pressed(pos_action):
+		_on_right_pressed()
+		
+	if Input.is_action_just_pressed(neg_action):
+		_on_left_pressed()
+
 
 func on_outfit_change():
 	var res = GameState.player_inventory.get_accessory(type)
