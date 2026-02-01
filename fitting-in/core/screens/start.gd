25be15d56d7e6bf7068a01171ae4db513879acdf
Shaydas1 <43556419+Shaydas1@users.noreply.github.com>
2026-02-01 14:51:07 -0500

    Title, end, controls screen fully done

--- fitting-in/core/screens/start.gd
+++ fitting-in/core/screens/start.gd
@@ -1,12 +1,10 @@
 extends Node2D
 
-func _on_button_pressed() -> void:
+func _on_start_button_pressed() -> void:
 	SceneManager.swap_screen(SceneManager.Screen.Game)
-
 
 func _on_credits_button_pressed() -> void:
 	SceneManager.swap_screen(SceneManager.Screen.End)
-
 
 func _on_controls_button_pressed() -> void:
 	SceneManager.swap_screen(SceneManager.Screen.Controls)
