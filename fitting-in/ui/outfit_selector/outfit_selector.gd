524a8e66596f96e6863e8811e84568f4b493f2d3
Lex Stapleton <lex.stapleton1423@gmail.com>
2026-02-01 15:58:21 -0500

    Update outfit_selector.gd

--- fitting-in/ui/outfit_selector/outfit_selector.gd
+++ fitting-in/ui/outfit_selector/outfit_selector.gd
@@ -4,7 +4,5 @@ class_name OutfitSelector extends Control
 
 func _input(event):
 	if event.is_action_pressed("open_outfit_select"):
-		panel.visible = true
+		panel.visible = not panel.visible
 	
-	if event.is_action_released("open_outfit_select"):
-		panel.visible = false
